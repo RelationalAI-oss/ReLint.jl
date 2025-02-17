1a2599bf9704ea8282024a13a573784da3010e78
Alexandre Bergel <alexandre.bergel@relational.ai>
2025-02-17 09:20:33 +0100

    Granting `RAI_FrontIR` to use the Shape API

--- src/linting/extended_checks.jl
+++ src/linting/extended_checks.jl
@@ -558,6 +558,7 @@ function check(t::NonFrontShapeAPIUsageRule, x::EXPR, markers::Dict{Symbol,Strin
     contains(markers[:filename], "src/FFI") && return
     contains(markers[:filename], "src/FrontIR") && return
     contains(markers[:filename], "packages/Shapes") && return
+    contains(markers[:filename], "packages/RAI_FrontIR") && return
     # Also, allow usages in tests
     contains(markers[:filename], "test/") && return
     # Also, allow usages of the name `Shape` in `packages/` although they refer to a different thing.

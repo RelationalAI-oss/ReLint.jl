de60a0cc6fc9f434864e6b699d6e6b67e3e0dc17
Alexandre Bergel <alexandre.bergel@relational.ai>
2025-10-20 14:50:17 +0200

    Added TodoJiraIssueRule

--- src/linting/text_lint_rules.jl
+++ src/linting/text_lint_rules.jl
@@ -5,9 +5,16 @@ abstract type LineFatalLintRule <: LineLintRule end
 
 # Line rules
 struct TodoPrRule <: LineFatalLintRule end
+struct TodoJiraIssueRule <: LineViolationLintRule end
 
 function check(t::TodoPrRule, line::String, markers::Dict{Symbol,String})
-    !isnothing(match(r"TODO\s?\(PR\)", line)) && return true, "`TODO (PR)` found, use `TODO` instead."
-    !isnothing(match(r"TODO PR", line)) && return true, "`TODO PR` found, use `TODO` instead."
+    !isnothing(match(r"TODO\s?\(PR\)", line)) && return true, "`TODO (PR)` found, use `TODO (RAI-XXXXX)` instead to refer to a Jira issue."
+    !isnothing(match(r"TODO PR", line)) && return true, "`TODO PR` found, use `TODO (RAI-XXXXX)` instead to refer to a Jira issue."
+    return false, ""
+end
+
+function check(t::TodoJiraIssueRule, line::String, markers::Dict{Symbol,String})
+    (contains(line, "TODO") && !contains(line, r"TODO\s?\(RAI-\d+\)")) &&
+        return true, "`TODO` found, use `TODO (RAI-XXXXX)` instead to refer to a Jira issue."
     return false, ""
 end
--- test/line_rules_tests.jl
+++ test/line_rules_tests.jl
@@ -1,23 +1,49 @@
 @testset "Line rule tests" begin
-    using ReLint: LintContext, TodoPrRule
+    @testset "TodoPrRule tests" begin
+        using ReLint: LintContext, TodoPrRule
 
-    context = LintContext([TodoPrRule])
-    @test lint_has_error_test("function f()\n # TODO (PR): fix this\n end"; context)
-    @test lint_test("function f()\n # TODO (PR): fix this\n end",
-            "Line 2, column 1: `TODO (PR)` found, use `TODO` instead.")
+        context = LintContext([TodoPrRule])
+        @test lint_has_error_test("function f()\n # TODO (PR): fix this\n end"; context)
+        @test lint_test("function f()\n # TODO (PR): fix this\n end",
+                "Line 2, column 1: `TODO (PR)` found, use `TODO (RAI-XXXXX)` instead to refer to a Jira issue.")
 
-    @test lint_has_error_test("function f()\n # TODO PR: fix this\n end"; context)
-    @test lint_test("function f()\n # TODO PR: fix this\n end",
-            "Line 2, column 1: `TODO PR` found, use `TODO` instead.")
+        @test lint_has_error_test("function f()\n # TODO PR: fix this\n end"; context)
+        @test lint_test("function f()\n # TODO PR: fix this\n end",
+                "Line 2, column 1: `TODO PR` found, use `TODO (RAI-XXXXX)` instead to refer to a Jira issue.")
 
-    @test !lint_has_error_test("function f()\n # TODO: fix this\n end"; context)
-    @test !lint_has_error_test("function f()\n # fix this\n end"; context)
+        @test !lint_has_error_test("function f()\n # TODO: fix this\n end"; context)
+        @test !lint_has_error_test("function f()\n # fix this\n end"; context)
 
-    @test lint_test("function f()\n @info \"zork\" # TODO PR: fix this\n end",
-            "Line 2, column 2: Unsafe logging statement. You must enclose variables and strings with `@safe(...)`.")
+        @test lint_test("function f()\n @info \"zork\" # TODO PR: fix this\n end",
+                "Line 2, column 2: Unsafe logging statement. You must enclose variables and strings with `@safe(...)`.")
 
-    @test lint_test("function f()\n @info \"zork\" # TODO PR: fix this\n end",
-            "Line 2, column 1: `TODO PR` found, use `TODO` instead.")
+        @test lint_test("function f()\n @info \"zork\" # TODO PR: fix this\n end",
+                "Line 2, column 1: `TODO PR` found, use `TODO (RAI-XXXXX)` instead to refer to a Jira issue.")
 
-    @test !lint_has_error_test("function f()\n # TODO probably okay\n end")
+        @test !lint_has_error_test("function f()\n # TODO probably okay\n end"; context)
+    end
+
+     @testset "TodoPrRule tests" begin
+        using ReLint: LintContext, TodoJiraIssueRule
+
+        context = LintContext([TodoJiraIssueRule])
+        @test lint_has_error_test("function f()\n # TODO (PR): fix this\n end"; context)
+        @test lint_test("function f()\n # TODO (PR): fix this\n end",
+                "Line 2, column 1: `TODO` found, use `TODO (RAI-XXXXX)` instead to refer to a Jira issue.")
+
+        @test lint_has_error_test("function f()\n # TODO: fix this\n end"; context)
+        @test lint_test("function f()\n # TODO: fix this\n end",
+                "Line 2, column 1: `TODO` found, use `TODO (RAI-XXXXX)` instead to refer to a Jira issue.")
+
+        @test !lint_has_error_test("function f()\n # TODO (RAI-12314): fix this\n end"; context)
+        @test !lint_has_error_test("function f()\n # fix this\n end"; context)
+
+        @test lint_test("function f()\n @info \"zork\" # TODO: fix this\n end",
+                "Line 2, column 2: Unsafe logging statement. You must enclose variables and strings with `@safe(...)`.")
+
+        @test lint_test("function f()\n @info \"zork\" # TODO fix this\n end",
+                "Line 2, column 1: `TODO` found, use `TODO (RAI-XXXXX)` instead to refer to a Jira issue.")
+
+        @test !lint_has_error_test("function f()\n # TODO (RAI-121) probably okay\n end")
+    end
 end

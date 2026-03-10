450b810be0ebd97167175fbbc159529fe74dcdec
Iulia Dumitru <iuliadmtru@gmail.com>
2026-03-10 13:07:20 +0100

    Move tests for fatal violations to dedicated file

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,8 +6,8 @@ include(joinpath(@__DIR__, "common.jl"))
 
 # include(joinpath(@__DIR__, "line_rules_tests.jl"))
 # include(joinpath(@__DIR__, "file_exclusion_tests.jl"))
-include(joinpath(@__DIR__, "noinline_tests.jl"))
-include(joinpath(@__DIR__, "safe_logging_tests.jl"))
+include(joinpath(@__DIR__, "fatal.jl"))
+
 # include(joinpath(@__DIR__, "lint_context_tests.jl"))
 # include(joinpath(@__DIR__, "rai_rules_tests.jl"))
 # include(joinpath(@__DIR__, "test_new_rules.jl"))

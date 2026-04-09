9c924d3443c6a43893e9cb206f78b61ace21e6cb
Iulia Dumitru <iuliadmtru@gmail.com>
2026-04-09 18:07:43 +0200

    Update local script

--- scripts/run_locally.sh
+++ scripts/run_locally.sh
@@ -3,32 +3,33 @@
 # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 # Run lint on a set of files, provided as arguments.
 # Result are printed in the stdout, if a fatal violation is found, then exit with 1
-# A rule can be specified using --rule NAME_OF_A_RULE
+# A rule can be specified using --rule RULE
 # If no rule is provided, then run all the rules.
 
 # EXAMPLE1:
 
 # pwd
-# /Users/alexandrebergel/Documents/RAI/ReLint.jl
+# .../ReLint.jl
 
-# ./scripts/run_lint_locally.sh /Users/alexandrebergel/Documents/RAI/raicode21/src/*.jl
-# FULLNAME SCRIPT ./scripts/run_lint_locally.sh
+# ./scripts/run_locally.sh dummy.jl
+# FULLNAME SCRIPT ./scripts/run_locally.sh
 # RELINT PATH= ./scripts/..
-# CURRENT PATH= /Users/alexandrebergel/Documents/RAI/ReLint.jl
+# CURRENT PATH= .../ReLint.jl
 # FILES_TO_RUN= /var/folders/nz/1c4rst196ws_18tjtfl0yb980000gn/T/tmp.SloaMkSs16
-# About to run ReLint.jl...
+# About to run ReLint...
 # [ Info: Running lint on 2 files
-# Line 47, column 9: Unsafe logging statement. You must enclose variables and strings with `@safe(...)`. /Users/alexandrebergel/Documents/RAI/raicode21/src/version.jl
+# Line 47, column 9: Unsafe logging statement. You must enclose variables and strings with
+# `@safe(...)`. dummy.jl
 # 6 potential threats are found: 1 fatal violation, 5 violations and 0 recommendation
 # Note that the list above only show fatal violations
 # ┌ Error: Fatal error discovered
 # └ @ Main none:25
 
 # EXAMPLE2:
-# ./scripts/run_julia_lint.sh /Users/alexandrebergel/Documents/RAI/raicode4/ --rule LogStatementsMustBeSafe
+# ./scripts/run_locally.sh dummy/ --rule VIOLATIONS["@async"]
 
 # EXAMPLE3:
-# ./scripts/run_julia_lint.sh /Users/alexandrebergel/Documents/RAI/raicode4/
+# ./scripts/run_locally.sh dummy/
 
 # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
@@ -39,7 +40,7 @@ while [[ $# -gt 0 ]]; do
   # echo "DEBUG: " $1
   case $1 in
     -r|--rule)
-      RULE+="\"$2\", "
+      RULE+="ReLint.$2, "
       shift # past argument
       shift # past value
       ;;
@@ -90,7 +91,7 @@ echo "RULE                            = $RULE"
 echo "RELINTPATH PATH                 =" $RELINTPATH
 echo "FILES_TO_RUN                    =" $FILES_TO_RUN
 
-echo "About to run ReLint.jl..."
+echo "About to run ReLint..."
 MAX_RETRIES=5
 RETRY_DELAY=5
 ATTEMPT=0
@@ -100,7 +101,7 @@ ATTEMPT=0
 # So we have a retry mechanism.
 while [[ $ATTEMPT -lt $MAX_RETRIES ]]; do
   ATTEMPT=$((ATTEMPT + 1))
-  echo "Attempt $ATTEMPT of $MAX_RETRIES to run ReLint.jl..."
+  echo "Attempt $ATTEMPT of $MAX_RETRIES to run ReLint..."
 
   julia --startup-file=no --history-file=no --project=$RELINTPATH -e "
     import Pkg
@@ -122,13 +123,14 @@ while [[ $ATTEMPT -lt $MAX_RETRIES ]]; do
 
     formatter = ReLint.PreCommitFormat()
     context = LintContext($RULE)
-    @info \"context\" context
+    rule_names = [r.name for r in context.rules]
+    @info \"rules\" rule_names
 
     for f in all_files
       ReLint.run_lint(f; result, formatter, context)
     end
 
-    if result.fatalviolations_count > 0
+    if result.fatal_violations_count > 0
       ReLint.print_summary(formatter, stdout, result)
       @error \"Fatal error discovered\"
       exit(1)
@@ -140,18 +142,18 @@ while [[ $ATTEMPT -lt $MAX_RETRIES ]]; do
   EXIT_STATUS=$?
 
   if [[ $EXIT_STATUS -eq 0 ]]; then
-    echo "ReLint.jl completed successfully."
+    echo "ReLint completed successfully."
     exit 0
   elif [[ $EXIT_STATUS -eq 1 ]]; then
-    echo "ReLint.jl found fatal violations"
+    echo "ReLint found fatal violations"
     exit 1
   else
-    echo "ReLint.jl failed with exit code $EXIT_STATUS. Retrying in $RETRY_DELAY seconds..."
+    echo "ReLint failed with exit code $EXIT_STATUS. Retrying in $RETRY_DELAY seconds..."
     sleep $RETRY_DELAY
   fi
 done
 
 if [[ $ATTEMPT -eq $MAX_RETRIES ]]; then
-  echo "ReLint.jl failed after $MAX_RETRIES attempts."
+  echo "ReLint failed after $MAX_RETRIES attempts."
   exit $EXIT_STATUS
 fi
